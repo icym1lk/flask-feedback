afdca3c0fdc0443206d370e7721f4a8390de7fc9
Jason Hanrahan <jasonhanr@gmail.com>
2020-06-01 11:24:47 -0400

    Add missing ] on username of AddUserForm

--- forms.py
+++ forms.py
@@ -5,7 +5,7 @@ from wtforms.validators import InputRequired, Email, Optional
 class AddUserForm(FlaskForm):
     """Form for adding users."""
 
-    username = StringField("Username", validators=[InputRequired(), Length(max=20, message="Must be 20 characters or less"))
+    username = StringField("Username", validators=[InputRequired(), Length(max=20, message="Must be 20 characters or less")])
     password = PasswordField("Password")
     email = StringField("Email", validators=[Email(message="Must enter a valid email"), Length(max=50, message="Must be 50 characters or less")])
     first_name = StringField("First Name", validators=[Length(min=1, max=30, message="Must be between 1 and 30 characters")])
