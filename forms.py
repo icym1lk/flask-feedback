0a65a3eea3d907ef636599776bffef7d9196cdbb
Jason Hanrahan <jasonhanr@gmail.com>
2020-06-01 11:51:40 -0400

    Email validation on AddUserForm works

--- forms.py
+++ forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
+from wtforms.fields.html5 import EmailField
 from wtforms.validators import InputRequired, Email, Length
 import email_validator
 
@@ -8,6 +9,6 @@ class AddUserForm(FlaskForm):
 
     username = StringField("Username", validators=[InputRequired(), Length(max=20, message="Must be 20 characters or less")])
     password = PasswordField("Password")
-    email = StringField("Email", validators=[InputRequired(), Length(max=50, message="Must be 50 characters or less")])
+    email = EmailField("Email", validators=[InputRequired(), Length(max=50, message="Must be 50 characters or less")])
     first_name = StringField("First Name", validators=[InputRequired(), Length(min=1, max=30, message="Must be between 1 and 30 characters")])
     last_name = StringField("Last Name", validators=[InputRequired(), Length(min=1, max=30, message="Must be between 1 and 30 characters")])
