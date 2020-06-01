1969f2fbcbbc22d2d0b43c6f3362bb6ad72459b7
Jason Hanrahan <jasonhanr@gmail.com>
2020-06-01 11:17:14 -0400

    Remove Optional validator from first and last name on AddUserForm

--- forms.py
+++ forms.py
@@ -8,5 +8,5 @@ class AddUserForm(FlaskForm):
     username = StringField("Username", validators=[InputRequired(), Length(max=20, message="Must be 20 characters or less"))
     password = PasswordField("Password")
     email = StringField("Email", validators=[Email(message="Must enter a valid email"), Length(max=50, message="Must be 50 characters or less")])
-    first_name = StringField("First Name", validators=[Length(min=1, max=30, message="Must be between 1 and 30 characters"), Optional()])
-    last_name = StringField("Last Name", validators=[Length(min=1, max=30, message="Must be between 1 and 30 characters"), Optional()])
+    first_name = StringField("First Name", validators=[Length(min=1, max=30, message="Must be between 1 and 30 characters")])
+    last_name = StringField("Last Name", validators=[Length(min=1, max=30, message="Must be between 1 and 30 characters")])
