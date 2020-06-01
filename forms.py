d2bd075e1a2e2951081ff7cca0dfe955f710fa50
Jason Hanrahan <jasonhanr@gmail.com>
2020-06-01 11:53:45 -0400

    Add InputRequired validator to password on AddUserForm

--- forms.py
+++ forms.py
@@ -8,7 +8,7 @@ class AddUserForm(FlaskForm):
     """Form for adding users."""
 
     username = StringField("Username", validators=[InputRequired(), Length(max=20, message="Must be 20 characters or less")])
-    password = PasswordField("Password")
+    password = PasswordField("Password", validators=[InputRequired()])
     email = EmailField("Email", validators=[InputRequired(), Length(max=50, message="Must be 50 characters or less")])
     first_name = StringField("First Name", validators=[InputRequired(), Length(min=1, max=30, message="Must be between 1 and 30 characters")])
     last_name = StringField("Last Name", validators=[InputRequired(), Length(min=1, max=30, message="Must be between 1 and 30 characters")])
